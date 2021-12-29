db59d2bd5dfb55f53b398c1199bc0dbabeb20747
navleryuu <elvandito36@gmail.com>
2021-12-29 13:12:01 +0700

    first commit

---
+++ js/musik-list.js
@@ -0,0 +1,51 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: 'Centimilimental - Kizuato',
+    artist: 'Centimilimental',
+    img: 'img_1',
+    src: 'musik-1',
+  },
+  {
+    name: 'Centimilimental – I Want To Die',
+    artist: 'Centimilimental',
+    img: 'img_2',
+    src: 'musik-2',
+  },
+  {
+    name: 'Centimilimental - Lupa Judul',
+    artist: 'Centimilimental',
+    img: 'img_3',
+    src: 'musik-3',
+  },
+  {
+    name: 'Centimilimental - YorugaAkeru',
+    artist: 'Centimilimental',
+    img: 'img_4',
+    src: 'musik-4',
+  },
+  {
+    name: 'Centimilimental - Urugawa No Sonzai',
+    artist: 'Centimilimental',
+    img: 'img_5',
+    src: 'musik-5',
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
